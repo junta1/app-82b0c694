3b0a8b8209a7d180a1a9333e734b410267677de8
Rafael Mattos <rafael_xuvisco@yahoo.com.br>
2021-09-24 23:36:48 -0300

    Adicionado migration e model para product e product moviment.

---
+++ api/database/migrations/2021_09_25_014755_product_moviment_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ProductMovimentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_moviment', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('sku');
+            $table->timestamps();
+
+            $table->foreign('sku')->references('sku')->on('product');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_moviment');
+    }
+}
