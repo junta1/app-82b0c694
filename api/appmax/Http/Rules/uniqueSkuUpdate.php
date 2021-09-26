a02405cbad64d7a9c771336a13328c2cf6f2756c
Rafael Mattos <rafael_xuvisco@yahoo.com.br>
2021-09-25 23:39:19 -0300

    Adicionado rules para sku.

---
+++ api/appmax/Http/Rules/uniqueSkuUpdate.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AppMax\Http\Rules;
+
+use AppMax\Repositories\ProductRepository;
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Facades\App;
+
+class uniqueSkuUpdate implements Rule
+{
+    protected $repository;
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->repository = App::make(ProductRepository::class);
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $uniqueSku = $this->repository->uniqueSku($value);
+
+        if(count($uniqueSku) > 1){
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'O campo sku é único.';
+    }
+}
